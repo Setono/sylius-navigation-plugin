faa4bc15b4131f6ea42bffc79d167e84db744b8b
Joachim Løvgaard <joachim@loevgaard.dk>
2025-03-31 15:18:10 +0200

    Made the max depth nullable. This makes a lot of sense because it allows you to render the whole navigation without being explicit

--- src/Form/Type/NavigationType.php
+++ src/Form/Type/NavigationType.php
@@ -6,6 +6,7 @@ namespace Setono\SyliusNavigationPlugin\Form\Type;
 
 use Sylius\Bundle\ResourceBundle\Form\EventSubscriber\AddCodeFormSubscriber;
 use Sylius\Bundle\ResourceBundle\Form\Type\AbstractResourceType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\FormBuilderInterface;
 
@@ -13,8 +14,16 @@ final class NavigationType extends AbstractResourceType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
-        $builder->add('maxDepth', IntegerType::class, [
-            'label' => 'setono_sylius_navigation.form.navigation.max_depth',
-        ])->addEventSubscriber(new AddCodeFormSubscriber());
+        $builder
+            ->add('maxDepth', IntegerType::class, [
+                'label' => 'setono_sylius_navigation.form.navigation.max_depth',
+                'required' => false,
+            ])
+            ->add('enabled', CheckboxType::class, [
+                'label' => 'sylius.ui.enabled',
+                'required' => false,
+            ])
+            ->addEventSubscriber(new AddCodeFormSubscriber())
+        ;
     }
 }
